494a833e8bfae1a2301b57aa11be7802aca0caff
emmanueluwa <emmanuel@fulodev.com>
2021-12-31 18:13:21 +0000

    using brownie to connect with external dependencies through github

---
+++ brownie_fundino/contracts/Fundino.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+// CONTRACT FOR A CROWD FUNDING APP
+// contract that accepts payments
+
+contract Fundino {
+    //uses safemath chainlink to stop any wrapping of int values
+    using SafeMathChainlink for uint256;
+
+    //keeping track of transactions
+    mapping(address => uint256) public addressToAmountFunded;
+
+    //creating an array for contract funders
+    address[] public funders;
+
+    //setting owner when smart contract is deployed, sender is contract deployer
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        //setting min value required, everything calculated in wei
+        uint256 minimumUSD = 50 * 10**18;
+        //revert error check and message
+        require(
+            getConversionRate(msg.value) >= minimumUSD,
+            "Please spend more ETH to continue"
+        );
+
+        //keywords used with every transaction [funding smart contract]
+        addressToAmountFunded[msg.sender] += msg.value;
+
+        //adding payer to list of funders
+        funders.push(msg.sender);
+    }
+
+    //to accept money in different currencies, conversion rates needed
+    function getVersion() public view returns (uint256) {
+        //using eth/usd address
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        return priceFeed.version();
+    }
+
+    //return price from the smart contract tuple
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        // blanks can be used if variable is available but unused
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 10000000000);
+        // 404577000000 = 4,045.77000000, smallest unit of measure Wei = 18d.p
+    }
+
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+    }
+
+    //withdrawing money to owners account
+    modifier onlyOwner() {
+        //making sure only owner can withdraw(call this function)
+        require(msg.sender == owner);
+        _;
+    }
+
+    function withdraw() public payable onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+
+        //updating each funder account updated to 0 when money withdrawed
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            //put address of funder inside funder variable
+            address funder = funders[funderIndex];
+
+            //use 'funder' for key in mapping, number of funders updated to 0
+            addressToAmountFunded[funder] = 0;
+        }
+        //resetting funders array
+        funders = new address[](0);
+    }
+}
