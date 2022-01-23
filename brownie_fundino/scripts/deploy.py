95614394bca9a77a01aad3935ab8914cf559fc9a
emmanueluwa <emmanuel@fulodev.com>
2022-01-23 13:50:47 +0000

    deploying test contract using local ganache network

--- brownie_fundino/scripts/deploy.py
+++ brownie_fundino/scripts/deploy.py
@@ -1,20 +1,16 @@
 from brownie import Fundino, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, deploy_mocks, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 
 def deploy_fundino():
     account = get_account()
     # if on persistant network eg rinkebey use address else deploy mocks
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
     else:
-        print(f"The active network is {network.show_active()}")
-        print("Deploying Mocks...")
-        #using v3 mock requires deciamls and initial ans
-        mock_aggregator = MockV3Aggregator.deploy(18, 2000000000000000000000, {"from": account})
-        price_feed_address = mock_aggregator.address
-        print("Mocks Deployed")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
         
-
+        
     # pass price feed address to fundino contract
     fundino_me = Fundino.deploy(price_feed_address, {"from": account}, publish_source=config["networks"][network.show_active()].get("verify"),)
     print(f"Contract deployed to {fundino_me.address}")
